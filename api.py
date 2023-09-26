cb37b34b0d5bcfc053c743ae2e11b81674ac08d6
Leon R <leon.rofagha@gmail.com>
2023-09-26 18:08:46 +0200

    added model.py; changed api.py to collect feel, rpe, id, start time

--- api.py
+++ api.py
@@ -5,12 +5,21 @@ import pandas as pd
 
 from config import USERNAME, API_KEY
 
-response = requests.get('https://intervals.icu/api/v1/athlete/i49168/activities.csv', auth=HTTPBasicAuth(USERNAME,API_KEY))
+response = requests.get(f'https://intervals.icu/api/v1/athlete/{API_KEY}/activities/?oldest=2022-02-06&newest=2023-09-27', auth=HTTPBasicAuth(USERNAME, API_KEY))
 print(response.status_code)
+data = response.json()
 
-filename = 'activities.csv'
-with open(filename, 'w') as file:
-    file.write(response.text)
+df = pd.DataFrame(data)
+df = df[['id', 'type', 'start_date_local', 'moving_time', 'icu_rpe', 'feel']]
+df.dropna(inplace=True)
+df.shape
+df.head()
+df.to_csv('activities.csv', index=False)
 
-activity_ids = pd.read_csv('/Users/leonrofagha/recovery_model/activities.csv', usecols=['id'])
-print(activity_ids.head())
+response2 = requests.get(f'https://intervals.icu//api/v1/activity/{API_KEY}/fit-file/?oldest=2023-09-26&newest=2023-09-27', auth=HTTPBasicAuth(USERNAME, API_KEY))
+print(response.status_code)
+fitfile = response.json()
+df2 = pd.DataFrame(fitfile)
+for col in df2.columns:
+    print(col)
+
