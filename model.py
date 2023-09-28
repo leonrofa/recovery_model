900ee3fd2c4f2fe3f4ce4edbee004269abf3ea01
Leon R <leon.rofagha@gmail.com>
2023-09-28 18:55:24 +0200

    more modifications to weekly plot (seasonal plot)

--- model.py
+++ model.py
@@ -25,17 +25,16 @@ plt.show()
 df['week_year'] = df.index.isocalendar().week.astype(str) + '_' + df.index.year.astype(str)
 df['dayweek'] = df.index.weekday
 
-grouped = df.groupby(['week_year', 'dayweek'])['feel'].mean().unstack()
-
 plt.figure(figsize=(10,6))
-for week_year in grouped.index:
-    plt.plot(grouped.columns, grouped.loc[week_year], marker='o', markerfacecolor='none', color='C0', alpha=0.25)
+for week_year in df['week_year'].unique():
+    week_data = df[df['week_year'] == week_year].sort_values(by='dayweek')
+    plt.plot(week_data['dayweek'], week_data['feel'], grouped.loc[week_year], marker='o', markerfacecolor='none', color='C0', alpha=0.25)
 
-plt.xticks(grouped.columns, ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'])
+plt.xticks(range(7), ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'])
 plt.xlabel('Day of the week')
 plt.ylabel('Feel (1: strong––5: weak)')
 plt.grid(True, which='both', linestyle='--', linewidth=0.5)
 plt.tight_layout()
 plt.show()
 
-# no clear seasonal effect
+# no clear weekly effect
