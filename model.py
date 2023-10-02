03539543fc6e8ca51321605618fc008192e1ced1
Leon R <leon.rofagha@gmail.com>
2023-10-02 16:57:57 +0200

    improved readability; processed time series to obtain regularly spaced, interpolated time series for 'feel'

--- model.py
+++ model.py
@@ -1,71 +1,66 @@
+import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from statsmodels.graphics.tsaplots import plot_acf
-from statsmodels.tsa.stattools import adfuller
-from statsmodels.tsa.stattools import q_stat
-from statsmodels.tsa.api import acf
-import matplotlib.pyplot as plt
-import numpy as np
+from statsmodels.tsa.stattools import adfuller, q_stat, acf
 
-# import dataframe
+# function definitions
+def compute_rmse(errors):
+    """compute the root mean square error"""
+    return np.sqrt(np.mean(np.square(errors)))
+
+def expanding_window_forecast(y, start_point):
+    """compute expanding-window residuals of 1-step ahead predictions"""
+    errors = []
+    for i in range(start_point, len(y)-1):
+        model = sm.tsa.ARIMA(y.iloc[:i+1], order=(1,0,1)).fit()
+        predicted_value = model.forecast(steps=1)[0]
+        errors.append(y.iloc[i+1]-predicted_value)
+    return errors
+
+# load dataframe
 df = pd.read_csv('activities.csv', parse_dates=['start_date_local'])
 df.set_index('start_date_local', inplace=True)
 df.rename(columns={'icu_rpe':'rpe'}, inplace=True)
-df.head()
 
-# ADF test, Ljung-Box test
-result_adf = adfuller(df['feel'])
+# filter and process data
+univariate_df = df.loc[df['type'].isin(['Elliptical', 'Run']), 'feel']
+univariate_df = univariate_df.resample('D').mean().interpolate(method='linear')
+
+# plot time series
+plt.figure(figsize=(12,4))
+plt.plot(univariate_df)
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+# adf and ljung-box tests
+result_adf = adfuller(univariate_df)
 print('# of lags:', result_adf[2])
 print('p-value:', result_adf[1])
 
 # reject that there's a unit root, conclude time series is stationary
 
-acf_vals = acf(df['feel'])
+acf_vals = acf(univariate_df)
 result_lb = q_stat(acf_vals, len(df['feel']))
 print('p-value:', result_lb[1][-1])
 
 # reject that the time series is white noise (iid)
 
-# model 1: linear least squares model
-X = df[['moving_time', 'rpe']]
-y = df['feel']
-
-m1 = LinearRegression()
-m1.fit(X, y)
-print(m1.coef_); print(m1.intercept_)
-print(m1.score(X, y))
-
-# model 2: ARMA(1,1) 
-m2 = sm.tsa.ARIMA(df['feel'], order=(1,0,1))
-m2_result = m2.fit()
+# autoregressive model for univariate time series
+m1 = sm.tsa.ARIMA(univariate_df, order=(1,0,1))
+m1_result = m1.fit()
 
 # plot residuals for model diagnostics
-fig = m2_result.plot_diagnostics(figsize=(12, 8))
+fig = m1_result.plot_diagnostics(figsize=(12, 8))
 plt.tight_layout()
 plt.show()
 
 # some deviations from normality in the tails; no significant autocorrelations
 
-# out-of-sample model evalution with expanding-window approach and RMSE 
-def compute_rmse(errors):
-    """compute the root mean square error"""
-    return np.sqrt(np.mean(np.square(errors)))
-
-def expanding_window_forecast(y, start_point, horizon=1):
-    """compute expanding-window residuals of h-step ahead predictions"""
-    errors = []
-    for i in range(start_point, len(y)-horizon):
-        model = sm.tsa.ARIMA(y.iloc(:i+1), order=(1,0,1)).fit()
-        predicted_value = model.forecast(steps=horizon)[0][-1]
-        errors.append(y.iloc[i+horizon]-predicted_value)
-    return errors
-
-y = pd.Series(df['feel'])
-
-start_point = round(0.75*len(y))
-errors = expanding_window_forecast(y, start_point)
+# out-of-sample model evaluation with expanding-window approach and RMSE
+start_point = round(0.7*len(univariate_df))
+errors = expanding_window_forecast(univariate_df, start_point)
 rmse = compute_rmse(errors)
-
-print(rmse)
+print(round(rmse, 4))
